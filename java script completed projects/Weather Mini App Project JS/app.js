3a8a6089c54c00c9ebef205fa254f0ab2ae28de0
davidbabaev198@gmail.com <davidbabaev198@gmail.com>
2024-08-15 17:59:10 +0300

    Moved all project files into docs folder for GitHub Pages

--- java script completed projects/Weather Mini App Project JS/app.js
+++
@@ -1,7 +0,0 @@
-const apiKey = "9c33218ec326c32521b9a43f76c95651";
-const apoUrl = "https://api.openweathermap.org/data/2.5/weather?q=moscow&units=metric";
-
-async function checkWeather(){
-    const response = await fetch();
-}
-
